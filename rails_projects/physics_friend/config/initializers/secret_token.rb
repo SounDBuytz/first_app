9c03db56b493701b10aa16fade45b01b970ce34f
Ron Bingham <bingham.ron@gmail.com>
2014-05-17 09:35:26 -0700

    Initial repository

---
+++ rails_projects/physics_friend/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhysicsFriend::Application.config.secret_key_base = '3b551514297b1c145a6c451e5c5f8423254e19b77241eaedf19fa3f14ebc75777b1b710a4388245722e548ef8db0d1e88c747d328bea7e02c3e7c354a0cdb484'
